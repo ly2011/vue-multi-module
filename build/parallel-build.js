a2a0e5e27623fdf88e1cb1cd8b28ca32fa572760
ex_luyun1 <yun2.lu@partner.midea.com>
2019-07-08 11:58:50 +0800

    <bug>: 恢复被追踪的文件

--- build/parallel-build.js
+++ build/parallel-build.js
@@ -29,7 +29,76 @@ utils.deployModule.forEach(module => {
 // const spinner = ora(`parallel building for production...\n`);
 // spinner.start();
 
-deleteDirs.forEach((path, index) => {
+// 删除dist文件夹下对应的模块文件
+const deleteDists = (deleteDirs = []) => {
+  const deletePromises = deleteDirs.map(
+    path =>
+      new Promise((resolve, reject) => {
+        rm(path, err => {
+          if (err) reject(err)
+          resolve()
+        })
+      })
+  )
+  return Promise.all(deletePromises)
+}
+
+// 开始执行webpack多模块多配置打包逻辑
+const initWebapck = () => {
+  const run = parallelWebpack.run
+  run(
+    require.resolve('./webpack.prod.parallel.conf.js'),
+    {
+      watch: false,
+      maxRetries: 1,
+      stats: true,
+      maxConcurrentWorkers: 2
+    },
+    function(err, stats) {
+      // spinner.stop();
+      let hasErrors = false
+      let statsArr = []
+      if (err && err.stats) {
+        statsArr.push(err.stats)
+      }
+      if (Array.isArray(stats)) {
+        statsArr = _.concat(statsArr, stats)
+      }
+      statsArr.forEach(stat => {
+        let rs = JSON.parse(stat)
+        if (Array.isArray(rs.errors) && rs.errors.length) {
+          hasErrors = true
+          rs.errors.forEach(err => {
+            console.log(chalk.yellow(err.toString()))
+          })
+        }
+      })
+      if (hasErrors) {
+        console.log(chalk.red('  Build failed with errors.\n'))
+        process.exit(1)
+      }
+
+      console.log(chalk.cyan('  Build complete.\n'))
+      console.log(
+        chalk.yellow(
+          '  Tip: built files are meant to be served over an HTTP server.\n' +
+            "  Opening index.html over file:// won't work.\n"
+        )
+      )
+      process.exit(0)
+    }
+  )
+}
+
+const init = async () => {
+  await deleteDists()
+  initWebapck()
+}
+
+// 开始执行
+init()
+
+/* deleteDirs.forEach((path, index) => {
   rm(path, err => {
     if (err) throw err
     if (index === deleteDirs.length - 1) {
@@ -78,4 +147,4 @@ deleteDirs.forEach((path, index) => {
       )
     }
   })
-})
+}) */
